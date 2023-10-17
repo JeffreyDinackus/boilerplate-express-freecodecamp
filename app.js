978193da281122d4d09c66a3021ea8e84cb6c011
JeffreyDinackus <jeffreydinackus@proton.me>
2023-10-17 19:06:03 -0400

    ready to deploy

--- app.js
+++ app.js
@@ -7,16 +7,12 @@ app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.send("<h1>hello Express!</h1>")
 })
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000,
   () => console.log("Server is running..."));
-
-
-
-
 
 
 
